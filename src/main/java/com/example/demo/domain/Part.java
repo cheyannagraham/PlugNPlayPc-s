612dfd3772e7e0bee904d1d70114cdc590e9bbeb
Cheyanna <cheyanna.graham@gmail.com>
2024-11-14 11:02:01 -0800

    add min & max fields

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -28,6 +28,8 @@ public abstract class Part implements Serializable {
     double price;
     @Min(value = 0, message = "Inventory value must be positive")
     int inv;
+    int minInv;
+    int maxInv;
 
     @ManyToMany
     @JoinTable(name="product_part", joinColumns = @JoinColumn(name="part_id"),
@@ -48,6 +50,22 @@ public abstract class Part implements Serializable {
         this.name = name;
         this.price = price;
         this.inv = inv;
+    }
+
+    public int getMinInv() {
+        return minInv;
+    }
+
+    public void setMinInv(int minInv) {
+        this.minInv = minInv;
+    }
+
+    public int getMaxInv() {
+        return maxInv;
+    }
+
+    public void setMaxInv(int maxInv) {
+        this.maxInv = maxInv;
     }
 
     public long getId() {
